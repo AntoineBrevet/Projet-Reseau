eae1e9a355f9cabb29ed9d9959167bb7d96672bb
safia <safia.medjahed@gmail.com>
2022-02-08 09:35:28 +0100

    start coding

---
+++ inc/header.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="apple-touch-icon" sizes="180x180" href="assets/ico/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="assets/ico/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="assets/ico/favicon-16x16.png">
+  <link rel="manifest" href="assets/ico/site.webmanifest">
+  <script src="https://kit.fontawesome.com/ceee3d5b7f.js" crossorigin="anonymous"></script>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Cabin:wght@400;500;600;700&display=swap" rel="stylesheet">
+
+  <link rel="stylesheet" href="assets/css/style.css">
+  <title>FrameIP</title>
+</head>
+
+<body>
+  <header>
+    <div class="header-container">
+      <div class="header-left">
+        <a href="index.php">
+          <img class="img-header" src="assets/img/logo.png" alt="">
+        </a>
+      </div>
+      <div class="header-right">
+        <i class="far fa-user-circle"></i>
+      </div>
+    </div>
+  </header>
---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+include('inc/header.php');
+
+?>
+
+<main>
+  <div class="container-body">
+    <div class="top-body">
+      <h1 class="body-title">FrameIP un choix évident pour analyser vos réseaux !</h1>
+    </div>
+    <div class="middle-body">
+      <h2 class="title-services">Services</h2>
+      <p class="text-services">ESN spécialisée en Infrastructure Réseau & Sécurité Informatique,
+        vous accompagne pour augmenter l’efficience de votre réseau,
+        par l’optimisation et la sécurisation des échanges.
+      </p>
+      <div class="img-middle-body">
+        <img class="img-middle-left" src="assets/img/connected.svg" alt="">
+      </div>
+    </div>
+  </div>
+</main>
