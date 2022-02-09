533495b5cbbaa17d692a98b95ba514b5e3a32e1a
wtanuki <97055775+wtanuki@users.noreply.github.com>
2022-02-09 14:40:16 +0100

    charts (suite)

---
+++ get_ICMP_TTL.php
@@ -0,0 +1,12 @@
+<?php
+include("inc/pdo.php");
+include("fonctions.php");
+
+$query = "SELECT ttl FROM res_trames WHERE protocol_name = 'ICMP'";
+$result = $pdo->query($query);
+$data = array();
+foreach($result as $row){
+   $data[] = $row;
+}
+print(json_encode($data));
+?>
